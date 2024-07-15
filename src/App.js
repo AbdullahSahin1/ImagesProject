7d0d8e4041b982a6a55443f64fb069f08296eb0d
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-07-15 21:29:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import SearchHeader from './SearchHeader';
-
+import searchImages from './Api';
 function App() {
-    const handleSubmit = (term)=>{  
+
+
+
+  const handleSubmit = (term)=>{  
     console.log(term);
+    searchImages(term)
   }
   return (
     <div className="App">
         <SearchHeader search={handleSubmit}/>
+        <searchImages/>
     </div>
   );
 }
